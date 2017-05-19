eed7876531a15b9eb175a2d50a5f676778912e91
Jae Huh <jaeeunhuh@gmail.com>
2017-05-19 14:08:44 +1200

    Delete lines to avoid merge conflict

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,8 @@
 import React from 'react'
-// import {HashRouter as Router, Route} from 'react-router-dom'
-
-import HeaderLeftBar from './HeaderLeftBar'
-import Genre from './Genre'
-import FinalPage from './FinalPage'
 
 const App = () => {
   return (
-    // <Router>
-      <div class="main-container">
-        <HeaderLeftBar />
-        {/* <h1>React development has begun!</h1>  */}
-        {/* <Route path='/home/q1' component={Genre} /> */}
-        {/* <Genre /> */}
-        <FinalPage />
-      </div>
-    // </Router>
+    <h1>React development has begun!</h1>
   )
 }
 
